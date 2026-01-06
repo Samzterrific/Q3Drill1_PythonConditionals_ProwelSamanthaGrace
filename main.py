fb213e0196002b4925d18b780efc90e5e77c6ca0
Samzterrific <prowel.sm@obmontessori.edu.ph>
2026-01-06 13:39:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
-from pyscript import document # pyright: ignore[reportMissingImports]
+from pyscript import document  # pyright: ignore[reportMissingImports]
 
-def compute_average(event) :
-    #Get the input values and convert to float
+def compute_average(event):
+    # Get the input values and convert to float
     score1 = float(document.getElementById("score1").value)
     score2 = float(document.getElementById("score2").value)
 
-    # Compute Average
+    # Compute average
     average = (score1 + score2) / 2
 
-    # Determine if Pass/Fail
-    if average >=75:
-        result = "Yes"
+    # Determine pass or fail
+    if average >= 75:
+        result = "Pass"
+    else:
+        result = "Fail"
 
-    else: 
-        result = "No"
-
-        # Display results
-        document.getElementById("average").innerText = str(round(average,2))
-        document.getElementById("result").innerText = result
+    # Display results
+    document.getElementById("average").innerText = str(round(average, 2))
+    document.getElementById("result").innerText = result
